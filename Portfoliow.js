5c1b7db2cc23e7991781ac00b2537cb3a2d705a2
Hardik Malviya <125384251+hardikm121@users.noreply.github.com>
2023-09-09 01:06:02 +0530

    Add files via upload

---
+++ Portfoliow.js
@@ -0,0 +1,51 @@
+
+function downloadCV() {
+    const cvURL = "C:\\Users\\HARDIK MALVIYA\\Downloads\\resume.pdf";
+    
+    window.open(cvURL, '_blank');
+  }
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll(".nav-menu a");
+
+    navLinks.forEach(function (link) {
+      link.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        navLinks.forEach(function (navLink) {
+          navLink.classList.remove("active");
+        });
+
+        link.classList.add("active");
+
+        const targetId = link.getAttribute("href").substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+          targetElement.scrollIntoView({ behavior: "smooth" });
+        }
+      });
+    });
+  });
+
+  function redirectToActualSite() {
+    const actualSiteURL = 'https://github.com/hardikm121';
+    window.location.href = actualSiteURL;
+  }
+
+  let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
